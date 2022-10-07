f0098b605d8e59d0bfda2840624ab81bdb65eecc
AhRemy <aruem8888@gmail.com>
2022-10-07 16:31:42 +0900

    내용 수정

--- src/main/java/com/example/testscheduler/TestSchedulerController.java
+++ src/main/java/com/example/testscheduler/TestSchedulerController.java
@@ -1,11 +1,15 @@
 package com.example.testscheduler;
 
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 @Slf4j
+@Component
+@EnableAsync 
 public class TestSchedulerController {
 	
 	 @Scheduled(cron = "0/10 * * * * *") // 0초부터 10초마다 실행
@@ -14,6 +18,7 @@ public class TestSchedulerController {
 	        lockAtLeastFor = "9s", // 락을 유지하는 시간을 설정한다. (9초로 설정했는데, 스케줄러 주기보다 약간 짧게 지정하는 것이 좋다.)
 	        lockAtMostFor = "9s" // 보통 스케줄러가 잘 동작하여 잘 종료된 경우 잠금을 바로 해제하게 되는데, 스케줄러 오류가 발생하면 잠금이 해제되지 않는다. 이런 경우 잠금을 유지하는 시간을 설정한다.
 	    )
+	 	
 	 
 	 	public void Schedule01()throws Exception {
 		 // 스케쥴 내용 
---
+++ src/test/java/com/example/testscheduler/Counter.java
@@ -0,0 +1,24 @@
+package com.example.testscheduler;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Counter {
+	
+	private AtomicInteger count = new AtomicInteger(0);
+	
+	@Scheduled(fixedDelay = 5)
+	public void scheduled() {
+		this.count.incrementAndGet();
+	}
+	
+	public int getInvocationCount() {
+		return this.count.get();
+	}
+	
+
+}
